4c99638f7ac456261f0e3805ae3cecc50f1056c3
Riletin <89656611+Riletin@users.noreply.github.com>
2024-11-18 01:43:09 -0600

    removed muted video code

--- assets/main.js
+++ assets/main.js
@@ -2226,7 +2226,6 @@
 								this.$video.src = this.src;
 								this.$video.poster = this.poster;
 								this.$video.autoplay = true;
-								this.$video.muted = true;
 								this.$video.preload = 'auto';
 								this.$video.loop = this.loop;
 								this.$video.playsInline = true;
